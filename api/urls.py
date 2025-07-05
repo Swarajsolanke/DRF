e4aa0440c9ec3d4961e44b5511c11102a19380f9
Swarajsolanke <swarajsolanke02@gmail.com>
2025-07-06 01:13:13 +0530

    viewsets added

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,22 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router=DefaultRouter()   #created the object Default routers 
+router.register("employees",views.EmployeeViewSet,basename="employee") #register this router 
 
 urlpatterns=[
-    path("student/",views.student,name="student"),
+   # path("student/",views.student,name="student"),
     # function based url
-    path("student/<int:pk>/",views.studentdetail,name="studentdetail"),
+    #path("student/<int:pk>/",views.studentdetail,name="studentdetail"),
     #path("emply/", views.employee_detail,name="employee"), function based view 
 
     #class based url 
-    path("employee/",views.Employees.as_view()),
-    path("employees/<int:pk>/",views.Employeedetail.as_view()),
-    path("employees/",views.Employeedetail.as_view()),
+   # path("employee/",views.Employees.as_view()),
+    #path("employees/<int:pk>/",views.Employeedetail.as_view()),
+    #path("employees/",views.Employeedetail.as_view()),
+
+  # include the router urls 
+    path("",include(router.urls)),
 
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import JsonResponse,HttpResponse
 from app.models import Student
 from . serializers import StudentSerializer,EmployeeSerializer
@@ -8,13 +8,17 @@ from rest_framework.response import Response
 from employee.models import Employee
 from rest_framework.views import APIView #creating class based view 
 from django.http import Http404
-from rest_framework import mixins ,generics
+from rest_framework import mixins ,generics,viewsets
+
+
 """# manually serilization of query set 
 def student(request):
     student=(Student.objects.all())  #this response is coming in queryset which is not a dict object for json response
     student_list=(list(student.values())) #converted that in list and then returning in jsonResponse
     print(student_list)
     return JsonResponse(student_list,safe=False)
+"""
+
 """
 @api_view(["GET","POST"])
 def student(request):
@@ -77,7 +81,7 @@ def studentdetail(request, pk):
         student.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 #without using serilization module 
-""""
+
 @api_view(["GET"])    
 def employee_detail(request):
     if request.method=="GET":
@@ -85,9 +89,9 @@ def employee_detail(request):
         print(f"employees deatails:{employees.values()}")
         employe_details=list(employees.values())
         return JsonResponse(employe_details,safe=False)
-   """
 
-""""
+
+
 function based view 
 @api_view(["GET"])      #for rendering rest_framework api 
 def employee_detail(request):
@@ -180,6 +184,8 @@ class Employeedetail(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.De
     def delete(self,request,pk):
         return self.destroy(request,pk)
 """
+
+"""
 #class based view using generics 
 class Employees(generics.ListCreateAPIView):
     queryset = Employee.objects.all()
@@ -188,3 +194,39 @@ class Employeedetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Employee.objects.filter()
     serializer_class = EmployeeSerializer
     lookup_field="pk"  #this is used to get the primary key of the object
+
+"""
+#viewset help us to write the single class to handle CURD opeartion 
+class EmployeeViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset=Employee.objects.filter()
+        serializer=EmployeeSerializer(queryset, many=True)
+        return Response(serializer.data)
+    def create(self,request):
+        serializer=EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def retrieve(self,request,pk=None):
+        employee=get_object_or_404(Employee,pk=pk)
+        serializer=EmployeeSerializer(employee)
+        return Response(serializer.data)
+    def update(self, request, pk=None):
+        employee=get_object_or_404(Employee, pk=pk)
+        serializer=EmployeeSerializer(employee, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def patch(self, request, pk=None):
+        employee=get_object_or_404(Employee, pk=pk)
+        serializer=EmployeeSerializer(employee, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk=None):
+        employee=get_object_or_404(Employee, pk=pk)
+        employee.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
